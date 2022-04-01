c28fe7a7426239d14fa795655bf851a723a7590a
youngwind <75987901@qq.com>
2022-04-01 17:08:02 +0800

    feat: add buble sort

---
+++ packages/dsv-website/src/config/data-structure/array/get-size.js
@@ -0,0 +1,14 @@
+const code = `const array = dsv.create({
+type: 'array',
+data: [10, 8, 6, 4, 2, 1, 3, 5, 7, 9]
+})
+
+array.getSize()
+
+`;
+
+module.exports = {
+  'zh-CN': 'getSize',
+  'en-US': 'getSize',
+  code: code,
+};
--- packages/dsv-website/src/config/gallery/sorting/BublleSort.js
+++ packages/dsv-website/src/config/gallery/sorting/BublleSort.js
@@ -3,8 +3,15 @@ const code = `const array = dsv.create({
   data: [10, 8, 6, 4, 2, 1, 3, 5, 7, 9]
 })
 
+const len = array.getSize();
 
-array.sort( )
+for (let j = 1; j < len; j++) {
+  for (let i = 0; i < len - j; i++) {
+    if (array.get([i]) > array.get([i + 1])) {
+      array.swap(i, i + 1)
+    }
+  }
+}
 `;
 
 module.exports = {
