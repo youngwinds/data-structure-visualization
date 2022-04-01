8d677631e0ba2e4eb6a5f811bcaae1e51b9f9a2b
youngwind <75987901@qq.com>
2022-04-01 15:10:04 +0800

    feat: init gallery

---
+++ packages/dsv-website/src/config/gallery/sorting/BublleSort.js
@@ -0,0 +1,14 @@
+const code = `const array = dsv.create({
+  type: 'array',
+  data: [10, 8, 6, 4, 2, 1, 3, 5, 7, 9]
+})
+
+
+array.sort( )
+`;
+
+module.exports = {
+  'zh-CN': '冒泡排序',
+  'en-US': 'Bublle Sort',
+  code: code,
+};
--- packages/dsv-website/src/locales/zh-CN.ts
+++ packages/dsv-website/src/locales/zh-CN.ts
@@ -1,12 +1,12 @@
 import { generatorLocaleByLanguage } from '@dsv-website/utils/locale';
-import common from './common';
+import sider from './sider';
 import navbar from './menu';
 import ds from './data-structure';
 import ga from './gallery';
 import bc from './backtracking';
 
 export default {
-  ...generatorLocaleByLanguage(common, 'zh-CN'),
+  ...generatorLocaleByLanguage(sider, 'zh-CN'),
   ...generatorLocaleByLanguage(navbar, 'zh-CN'),
   ...generatorLocaleByLanguage(ds, 'zh-CN'),
   ...generatorLocaleByLanguage(ga, 'zh-CN'),
