f0d96d8fed2b32a49280dd70511904984c83902f
youngwind <75987901@qq.com>
2022-04-02 12:44:54 +0800

    feat: array add  shift and unshift, fix set bug

---
+++ packages/dsv-website/src/config/data-structure/array/shift.js
@@ -0,0 +1,15 @@
+const code = `const array = dsv.create({
+    type: 'array',
+    data: [10, 8, 6, 4, 2, 1, 3, 5, 7, 9]
+})
+
+array.shift()
+
+array.shift()
+`;
+
+module.exports = {
+  'zh-CN': 'shift',
+  'en-US': 'shift',
+  code: code,
+};
---
+++ packages/dsv-website/src/config/data-structure/array/unshift.js
@@ -0,0 +1,18 @@
+const code = `const array = dsv.create({
+    type: 'array',
+    data: [10, 8, 6, 4, 2, 1, 3, 5, 7, 9]
+})
+
+array.unshift(1)
+
+array.unshift(2, 3)
+
+array.unshift(4, 5, 6)
+
+`;
+
+module.exports = {
+  'zh-CN': 'unshift',
+  'en-US': 'unshift',
+  code: code,
+};
