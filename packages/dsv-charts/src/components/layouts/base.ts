0dcf61998c0d5d2e2ef6f5b3fc219e37b0c15fab
youngwind <75987901@qq.com>
2022-04-06 00:23:54 +0800

    feat: add position absolute for layout

--- packages/dsv-charts/src/components/layouts/base.ts
+++ packages/dsv-charts/src/components/layouts/base.ts
@@ -1,15 +1,20 @@
+import { Cartesian2LayoutConfigType } from '@dsv-charts/types';
 import { select, Selection } from 'd3-selection';
 
 export class BaseLayout {
   private _dom: HTMLElement;
+  private _config: Cartesian2LayoutConfigType;
   private _rootRect: DOMRect;
   private _container: Selection<HTMLElement, unknown, null, undefined>;
   private _svg: Selection<SVGSVGElement, unknown, null, undefined>;
 
-  constructor(dom: HTMLElement) {
+  constructor(dom: HTMLElement, config: Cartesian2LayoutConfigType) {
     this._dom = dom;
+    this._config = config;
 
-    this.initRect().initContainer().initSvg();
+    this.initContainer();
+    this.initRect();
+    this.initSvg();
   }
 
   public addGroup() {
@@ -17,7 +22,7 @@ export class BaseLayout {
   }
 
   private initRect() {
-    this._rootRect = this._dom.getBoundingClientRect();
+    this._rootRect = this._container.node().getBoundingClientRect();
     return this;
   }
 
@@ -25,11 +30,28 @@ export class BaseLayout {
    * 初始化svg图表容器
    */
   private initContainer() {
+    const height =
+      this._config.height ?? this._dom.getBoundingClientRect().height;
+    const width = this._config.width ?? this._dom.getBoundingClientRect().width;
+    const zIndex = this._config.zIndex ?? 1;
+    const position = this._config.position ?? 'relative';
+
+    const top = this._config.top ?? 0;
+    const right = this._config.right ?? 0;
+    const bottom = this._config.bottom ?? 0;
+    const left = this._config.left ?? 0;
+
     this._container = select(this._dom)
       .append('div')
-      .style('width', this._rootRect.width)
-      .style('height', this._rootRect.height)
-      .style('position', 'relative');
+      .style('height', `${height}px`)
+      .style('width', `${width}px`)
+      .style('position', position)
+      .style('z-index', zIndex)
+      .style('top', top ? `${top}px` : undefined)
+      .style('right', right ? `${right}px` : undefined)
+      .style('bottom', bottom ? `${bottom}px` : undefined)
+      .style('left', left ? `${left}px` : undefined);
+
     return this;
   }
 
@@ -43,6 +65,7 @@ export class BaseLayout {
       .attr('width', this._rootRect.width)
       .attr('height', this._rootRect.height)
       .attr('style', 'max-width: 100%; height: auto; height: intrinsic;');
+
     return this;
   }
 
--- packages/dsv-website/src/components/Chart/index.tsx
+++ packages/dsv-website/src/components/Chart/index.tsx
@@ -13,6 +13,7 @@ export function Chart() {
         style={{
           width: '100%',
           height: '100%',
+          position: 'relative',
         }}
       ></div>
     </div>
