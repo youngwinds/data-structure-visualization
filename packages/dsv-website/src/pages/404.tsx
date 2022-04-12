a431def381782eaaacda7949b89376646dc3f176
youngwind <75987901@qq.com>
2022-04-12 21:30:27 +0800

    feat: add 404 and default router

---
+++ packages/dsv-website/src/pages/404.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1 style={{ textAlign: 'center', padding: '100px 0' }}>404 Not Found!</h1>
+  );
+}
---
+++ packages/dsv-website/src/pages/index.tsx
@@ -0,0 +1,4 @@
+import { Redirect } from 'umi';
+export default function Page() {
+  return <Redirect to="/home" />;
+}
