74b967dee08cedc5245841a7840ac263b1e23c58
youngwind <75987901@qq.com>
2022-04-17 00:15:17 +0800

    feat: add merge sort

---
+++ packages/dsv-website/src/config/gallery/divide-and-conquer/merge-sort-once.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'zh-CN': '归并排序(一次)',
+  'en-US': 'Merge Sort Once',
+  path: '/docs/gallery/divide-and-conquer/merge-sort-once',
+};
---
+++ packages/dsv-website/src/config/gallery/divide-and-conquer/merge-sort.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'zh-CN': '归并排序',
+  'en-US': 'Merge Sort',
+  path: '/docs/gallery/divide-and-conquer/merge-sort',
+};
