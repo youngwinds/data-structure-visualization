9b21f785424263651abbf43f3f86f820d6ba1fb2
youngwind <75987901@qq.com>
2022-04-16 17:14:11 +0800

    feat: add quick \selection sort, dp tree

--- packages/dsv-website/public/docs/gallery/dynamic-programming/three-steps-problem/code.js
+++ packages/dsv-website/public/docs/gallery/dynamic-programming/three-steps-problem/code.js
@@ -1,14 +1,18 @@
-const stairsSize = 10;
+// const n = 12;
+const n = 7;
 
 const dp = dsv.create({
   type: 'array',
-  data: new Array(stairsSize).fill(0),
+  data: new Array(n).fill(0),
+  transition: {
+    duartion: 100,
+  },
 });
 
 dp.set(0, 1);
 dp.set(1, 2);
 dp.set(2, 4);
 
-for (let i = 3; i < stairsSize; i++) {
+for (let i = 3; i < n; i++) {
   dp.set(i, dp.get(i - 1) + dp.get(i - 2) + dp.get(i - 3));
 }
