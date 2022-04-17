42d0aa22af48e6c290fd2a32391d706a49e7d821
youngwinds <youngwinds@users.noreply.github.com>
2022-04-17 05:40:25 +0000

    deploy: 1bfdf116831a68ab958f64a67e2118eadbf68e9a

--- docs/gallery/sorting/quick-sort/code.js
+++ docs/gallery/sorting/quick-sort/code.js
@@ -1,6 +1,6 @@
 const array = dsv.create({
   type: 'array',
-  data: [10, 8, 6, 4, 2, 1, 3, 5, 7, 9],
+  data: [10, 8, 6, 4, 2, 1, 3, 9, 7, 5],
   state: {
     end: '#edafda',
     pivot: '#d87c7c',
