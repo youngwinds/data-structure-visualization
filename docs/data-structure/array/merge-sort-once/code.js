8c078c43b1216ecdaa300b2655fc518fc437bbfa
youngwinds <youngwinds@users.noreply.github.com>
2022-04-17 13:21:11 +0000

    deploy: 24e654d943fb387fcf201bf44134e6768ab9af7d

---
+++ docs/data-structure/array/merge-sort-once/code.js
@@ -0,0 +1,78 @@
+const tempArray = dsv.create({
+  type: 'array',
+  data: [],
+  transition: {
+    duration: 100,
+  },
+  layout: {
+    padding: {
+      left: 0,
+      right: 0,
+      bottom: 30,
+      top: 20,
+    },
+    height: 275,
+  },
+});
+
+const array = dsv.create({
+  type: 'array',
+  data: [6, 7, 8, 9, 10, 6, 7, 8, 9, 10],
+  state: {
+    end: '#edafda',
+    pivot: '#ffdd00',
+  },
+  transition: {
+    duration: 100,
+  },
+  layout: {
+    padding: {
+      left: 0,
+      right: 0,
+      bottom: 30,
+      top: 50,
+    },
+    height: 275,
+  },
+});
+
+function merge(arr, left, middle, right) {
+  let n1 = middle - left + 1;
+  let n2 = right - middle;
+
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p1 < n1 && p2 < n2) {
+    if (arr.get(left + p1) <= arr.get(middle + 1 + p2)) {
+      tempArray.push(arr.get(left + p1));
+      p1++;
+    } else {
+      tempArray.push(arr.get(middle + 1 + p2));
+      p2++;
+    }
+  }
+
+  while (p1 < n1) {
+    tempArray.push(arr.get(left + p1));
+    p1++;
+  }
+
+  while (p2 < n2) {
+    tempArray.push(arr.get(middle + 1 + p2));
+    p2++;
+  }
+
+  for (let index = right; index >= left; index--) {
+    arr.set(index, tempArray.pop());
+  }
+
+  // clear
+  tempArray.splice(0, tempArray.length);
+}
+
+const left = 0;
+const right = array.length - 1;
+const middle = ((right - left) >> 1) + left;
+
+merge(array, left, middle, right);
---
+++ docs/data-structure/array/quick-sort/code.js
@@ -0,0 +1,38 @@
+const array = dsv.create({
+  type: 'array',
+  data: [10, 8, 6, 4, 2, 1, 3, 9, 7, 5],
+  state: {
+    end: '#edafda',
+    pivot: '#d87c7c',
+  },
+  transition: {
+    duration: 100,
+  },
+});
+
+function partition(arr, low, high) {
+  let pivot = arr.get(high);
+  arr.setState(high, 'pivot');
+  let i = low - 1;
+
+  for (let j = low; j <= high - 1; j++) {
+    if (arr.get(j) < pivot) {
+      i++;
+      arr.swap(i, j);
+    }
+  }
+  arr.swap(i + 1, high);
+  arr.setState(i + 1, 'end');
+  return i + 1;
+}
+
+function quickSort(arr, low, high) {
+  if (low <= high) {
+    let pi = partition(arr, low, high);
+
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+}
+
+quickSort(array, 0, array.length - 1);
